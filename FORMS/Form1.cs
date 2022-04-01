76707f90f1f9bf7fc36c7ea4e78b1cd5e6030527
Somel Ahmed <89021855+ahmedsomel@users.noreply.github.com>
2022-04-01 23:46:07 +0600

    Add files via upload

---
+++ FORMS/Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LOGIN_FORM
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            PATIENT_LOGIN patient=new PATIENT_LOGIN();
+            patient.Show();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            panel1.BackColor=Color.FromArgb(100,0,0,0);
+        }
+
+        private void label2_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
